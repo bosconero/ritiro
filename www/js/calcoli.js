50e0cfc49dc91e5676bb9798a170936b3fc7bc1a
bosconero <angelonastasi@hotmail.com>
2016-06-29 11:22:31 +0200

    Add files via upload

--- www/js/calcoli.js
+++ www/js/calcoli.js
@@ -86,10 +86,10 @@ ritiromatur=0;
 ritirolastraz=ritirolastray+ritirostrato+ritiromatur;
 ritirolastra=Math.round(ritirolastraz*100)/100;
 var boxalert=document.getElementById('boxalert');
-boxalert.innerHTML="Il ritiro in fase plastica del calcestruzzo giovane per una distanza tra i giunti di contrazione pari a <span style='font-weight:bold;color:#ff2200;'>"+distcont+" ml </span> sara pari a <br /> <span style='font:bold 20px arial;color:#ff2200;display:block;width:100%;text-align:center;'>"+ritirolastra+" mm </span>";
+boxalert.innerHTML="Il ritiro teorico in fase plastica del calcestruzzo giovane  con giunti tagliati ogni <span style='font-weight:bold;color:#ff2200;'>"+distcont+" ml </span> sara pari a <br /> <span style='font:bold 20px arial;color:#ff2200;display:block;width:100%;text-align:center;'>"+ritirolastra+" mm </span>";
 var alertd=document.getElementById('alert2');
 if (ritirolastra>1.5){
-alertd.innerHTML="<span style='font:bold 16px arial;color:#ff2200;'>RICALCOLARE IL DIMENSIONAMENTO DEI GIUNTI PER RICONDURRE IL RITIRO ENTRO LIMITI ACCETTABILI</span>";
+alertd.innerHTML="<span style='font:bold 16px arial;color:#ff2200;'>RICALCOLARE IL DIMENSIONAMENTO DEI GIUNTI SINO AD OTTENERE UN RITIRO NON SUPERIORE A 1.5mm</span>";
 }
 var modelast=5700*Math.sqrt(rck);     
 var modelas=Math.round(modelast);         // modulo elastico calcestruzzo
@@ -100,8 +100,8 @@ var fattrit=(cespo*umi*srit)/1000000; //d4
 var sollmediatrazionex=(fattrit*modelas*(100*spess))/1500; 
 var sollmediatrazione=Math.round(sollmediatrazionex*100)/100;//sollecitazione media a trazione
 moduloelastico.innerHTML="Modulo elastico cls: <span style='font:bold 16px; color:#ff2200;'>"+modelas+" N/mm<sup>2</sup> </span>";
-resistenzaatrazione.innerHTML="Resistenza a trazione: <span style='font:bold 16px; color:#ff2200;'>"+resistrazione+" N/mm<sup>2</sup></span>";
-resistenzamediaatrazione.innerHTML="Soll. media a trazione: <span style='font:bold 16px; color:#ff2200;'>"+sollmediatrazione+" N/mm<sup>2</sup></span>";
+resistenzaatrazione.innerHTML="Resistenza a trazione N/mm<sup>2</sup>: <span style='font:bold 16px; color:#ff2200;'>"+resistrazione+" N/mm<sup>2</sup></span>";
+resistenzamediaatrazione.innerHTML="Soll. media a trazione N/mm<sup>2</sup>: <span style='font:bold 16px; color:#ff2200;'>"+sollmediatrazione+" N/mm<sup>2</sup></span>";
 
 
 }
